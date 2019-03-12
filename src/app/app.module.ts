3e9686da761327d94c6a51abd6dfe46f874b81c6
William Condori Quispe <williamccondori@outlook.com>
2019-03-11 20:04:55 -0500

    Se agregan paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
-import { ServicesComponent } from './services/services.component';
+import { ServiceComponent } from './service/service.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
+import { WhyComponent } from './why/why.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { TeamComponent } from './team/team.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     BannerComponent,
-    ServicesComponent,
+    ServiceComponent,
     AboutComponent,
-    HeaderComponent
+    HeaderComponent,
+    WhyComponent,
+    TestimonialComponent,
+    TeamComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
